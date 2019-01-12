93cc1e1b056b662a63ddd29e392334f7b0b9e501
Kchao1910 <kchao1910@gmail.com>
2019-01-11 23:08:57 -0800

    Edited: Output now correctly shows values to two decimal places

--- calculate.js
+++ calculate.js
@@ -1,14 +1,24 @@
 function calculate() {
-    
+
+    // 4 values must be retrieved in order to output correctly
+    // Category name retrieval
     var categoryValue = document.getElementById("categoryInput").value;
-    var budgetValue = document.getElementById("budgetInput").value;
-    var expenseValue = document.getElementById("expenseInput").value;
+    
+    // Budget input retieval
+    var budgetValue = parseFloat(document.getElementById("budgetInput").value).toFixed(2);
+
+    // Expense input retrieval
+    var expenseValue = parseFloat(document.getElementById("expenseInput").value).toFixed(2);
+
+    // Calculate budget left over
     var totalValue = budgetValue - expenseValue;
 
+    // Output: Category, Budget, Expenses, Total
     document.getElementById("finalCategory").innerHTML = ('Category name: ' + categoryValue);
     document.getElementById("finalBudget").innerHTML = ('Budget: $' + budgetValue);
     document.getElementById("finalExpense").innerHTML = ('Expense: $' + expenseValue);
 
+    // Determines whether user is under budget or overbudget
     if (totalValue >= 0) {
         document.getElementById("finalTotal").innerHTML = ('Total: $' + totalValue.toFixed(2));
     } else if (totalValue < 0) {
@@ -16,4 +26,16 @@ function calculate() {
     } else {
         document.getElementById("finalTotal").innerHTML = ("Whoops: looks like we missed an error.");
     }
+
+}
+
+// Note-to-self: Making the yes button disappear could limit options for the user. 
+// Example: User clicks on 'No' button by mistake and can no longer create a csv file.
+// Decision: 'No' button doesn't really do anything, thus, just leave both on in the future
+function noDisappear() {
+    document.getElementById("NB").style.display = "none";
+}
+
+function yesDisappear() {
+    document.getElementById("YB").style.display = "none";
 }
