2f79d15837f7cabb59014b34c3bca50289cf2da2
Kchao1910 <kchao1910@gmail.com>
2019-01-12 20:55:22 -0800

    Added: Ability to dynamically create new categories, budgets, and expenses

--- calculate.js
+++ calculate.js
@@ -24,8 +24,46 @@ function calculate() {
     } else if (totalValue < 0) {
         document.getElementById("finalTotal").innerHTML = ('Total: -$' + Math.abs(totalValue.toFixed(2)) + ' (You\'re overbudget by $' + Math.abs(totalValue.toFixed(2)) + ')');
     } else {
-        document.getElementById("finalTotal").innerHTML = ("Whoops: looks like we missed an error.");
+        document.getElementById("finalTotal").innerHTML = ("Whoops: looks like you didn't enter a value for one of the variables.");
     }
+
+
+    var newCat = document.createTextNode("Category");
+    var newBud = document.createTextNode("Budget");
+    var newExp = document.createTextNode("Expenses");
+
+    var inputElement = document.createElement("input");
+    var inputElement2 = document.createElement("input");
+    var inputElement3 = document.createElement("input");
+    var breakElement = document.createElement("br");
+    var breakElement2 = document.createElement("br");
+    var breakElement3 = document.createElement("br");
+
+    var cln = breakElement.cloneNode(true);
+    var cln2 = breakElement.cloneNode(true);
+    var cln3 = breakElement.cloneNode(true);
+    var cln4 = breakElement.cloneNode(true);
+    var cat = document.getElementById("categories");
+
+    cat.appendChild(newCat);
+    cat.appendChild(breakElement);
+    cat.appendChild(inputElement);
+
+    cat.appendChild(breakElement2);
+    cat.appendChild(newBud);
+    cat.appendChild(cln);
+    cat.appendChild(inputElement2);
+
+    cat.appendChild(breakElement3);
+    cat.appendChild(newExp);
+    cat.appendChild(cln2);
+    cat.appendChild(inputElement3);
+    cat.appendChild(cln3);
+    cat.appendChild(cln4);
+    
+    
+    
+    
 
 }
 
