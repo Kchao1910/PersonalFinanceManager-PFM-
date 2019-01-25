faf7871c55300d3fc46bbee88902f68b4a7ae2a8
Kchao1910 <kchao1910@gmail.com>
2019-01-24 23:48:26 -0800

    Added: goals page

---
+++ goalsPopulate.js
@@ -0,0 +1,10 @@
+function goalPop() {
+    removeAllNodes();
+
+    // parent node
+    var body = document.getElementById("parent");
+
+    var IP = document.createTextNode("Come back later, this page is still in development.");
+
+    body.appendChild(IP);
+}
